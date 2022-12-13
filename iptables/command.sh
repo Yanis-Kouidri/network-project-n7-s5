937d3a54b92092cb94def5520e13fe3c8179c910
Yanis Kouidri <yanis.kouidri@gmail.com>
2022-12-13 17:25:39 +0100

    DNS interne et externe OK !

---
+++ iptables/command.sh
@@ -0,0 +1,11 @@
+# Commandes iptable :
+
+# Connecter la dmz et la zone privée :
+sudo iptables -I DOCKER-ISOLATION-STAGE-2 -o br-cd9b849ae2de -i br-729e43e6a477 -j ACCEPT
+sudo iptables -I DOCKER-ISOLATION-STAGE-2 -o br-729e43e6a477 -i br-cd9b849ae2de -j ACCEPT
+
+# Empecher l'accès à internet depuis la zone privé :
+sudo iptables -I DOCKER-ISOLATION-STAGE-2 -o wlp0s20f3 -i br-cd9b849ae2de -j DROP
+sudo iptables -I DOCKER-ISOLATION-STAGE-2 -o br-cd9b849ae2de -i wlp0s20f3 -j DROP
+sudo iptables -t nat -A PREROUTING -p udp --dport 53 -j DNAT --to-destination 172.20.1.2
+
