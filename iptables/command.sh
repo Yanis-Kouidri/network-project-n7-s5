0d6d2a52482e4ca98eab1f228765ffa76d17ab7a
Yanis Kouidri <yanis.kouidri@gmail.com>
2022-12-13 18:44:16 +0100

    Ajout d'un proxy, tout est OK

--- iptables/command.sh
+++ iptables/command.sh
@@ -7,5 +7,7 @@ sudo iptables -I DOCKER-ISOLATION-STAGE-2 -o br-729e43e6a477 -i br-cd9b849ae2de 
 # Empecher l'accès à internet depuis la zone privé :
 sudo iptables -I DOCKER-ISOLATION-STAGE-2 -o wlp0s20f3 -i br-cd9b849ae2de -j DROP
 sudo iptables -I DOCKER-ISOLATION-STAGE-2 -o br-cd9b849ae2de -i wlp0s20f3 -j DROP
+
+# Faire du port forwarding sur tout ce qui arrive sur le port 53 vers le serveur DNS externe
 sudo iptables -t nat -A PREROUTING -p udp --dport 53 -j DNAT --to-destination 172.20.1.2
 
